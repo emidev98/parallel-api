08a1aa54b55f4da99f816dba3c33332f64e1e290
fmolins <frankii267mc@gmail.com>
2018-05-11 16:39:27 +0200

    Changed destination db for models

--- models/CryptoUser.js
+++ models/CryptoUser.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
-var conn2     = mongoose.createConnection('mongodb://localhost/testB');
+var conn2     = mongoose.createConnection('mongodb://localhost/cryptousers');
 
 var CryptoUsersSchema = new Schema({
     userid: ObjectId,
