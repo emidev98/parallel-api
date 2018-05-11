4147dd756940d87920091de63d07c40093420fa0
Marc Sala Manzano <salamarc6@gmail.com>
2018-05-11 16:32:18 +0200

    Routes function created

--- controllers/HelloWorldController.js
+++ controllers/HelloWorldController.js
@@ -1,19 +1,6 @@
-var express = require('express');
-// Utilitzem el Router de express per crear les rutes
-var router = express.Router();
+var express     = require('express');
+var HelloWorld  = require('../models/HelloWorld');
 
-//Serveix per agafar la info de la peticio de body i fer-la bonica (Middleware)
-var bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
-
-var HelloWorld = require('../models/HelloWorld');
-
-router.get('/', function(req, res){
-    HelloWorld.find({}, function (err, users) {
-        if(err) return res.status(500).send("There was a problem finding the users.");
-        res.status(200).send(users[0]);
-    });
-});
-
-module.exports = router;
+module.exports.getHelloWorld = function(callback){
+    HelloWorld.find({}, callback);
+}
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,13 +1,6 @@
 var express = require('express');
-// Utilitzem el Router de express per crear les rutes
-var router = express.Router();
+var User 	= require('../models/User');
 
-//Serveix per agafar la info de la peticio de body i fer-la bonica (Middleware)
-var bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
-
-var User = require('../models/User');
 
 router.get('/', function(req, res){
 	res.send('Hello world');
