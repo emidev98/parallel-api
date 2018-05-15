c1309be8aa38d34d47e94786bd97cf3ac36b0161
fmolins <frankii267mc@gmail.com>
2018-05-15 19:23:15 +0200

    Testing login end point

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var User 	= require('../models/User');
+var errorCodes = require('../errors/errorCodes');
+var CustomError = require('../errors/CustomError');
 
-module.exports.login = function(user, callback){
+/*module.exports.login = function(user, callback){
 	var userDB = User.findOne({
 		email: user.email
 	}, function (err, userDB){
@@ -20,8 +22,65 @@ module.exports.login = function(user, callback){
 				passwordNotMatchError.message = "The password doesn't match!";
 				callback(passwordNotMatchError, undefined);
 			} else {
-				callback(null, user);
+				var returnUser = {
+					token: userDB._id,
+					language: userDB.language,
+					email: userDB.email,
+					firstname: userDB.firstname,
+					lastname: userDB.lastname
+				}
+				callback(null, returnUser);
 			}
 		});
 	});
+}*/
+
+module.exports.login = function (user, callback){
+	console.log(user.email);
+	checkUserEmail(user)
+		.then(users => compareHash(users))
+		.then(userDB => createUserResponse(userDB))
+		.then(userDB => callback(null, userDB))
+		.catch(err => callback(err, undefined))
+}
+
+function checkUserEmail(user){
+	return new Promise(function (resolve, reject){
+		var userDB = User.findOne({
+			email: user.email
+		}, function (err, userDB){
+			if (err) callback(err, undefined);
+			if (!userDB){
+				var error = {
+					status: 404,
+					errorCode: errorCodes.INVALID_USER_OR_PASSWORD,
+					errorKey: "INVALID_USER_OR_PASSWORD"
+				};
+				var userNotFoundError = new CustomError(error);
+				return reject(userNotFoundError);
+			}
+			var users = [user, userDB];
+			resolve(users);
+		})
+	})
+}
+
+function compareHash(users){
+	var USER_FRONT_END = 0;
+	var USER_DB = 1;
+	return new Promise(function (resolve, reject){
+		bcrypt.compare(users[USER_FRONT_END].password, users[USER_DB].password, function(err, res) {
+			if (err) callback(err, undefined);
+			if (!res){
+				var error = {
+					status: 404,
+					errorCode: errorCodes.INVALID_USER_OR_PASSWORD,
+					errorKey: "INVALID_USER_OR_PASSWORD"
+				};
+				var passwordNotMatchError = new CustomError(error);
+				return reject(passwordNotMatchError);
+			}
+			resolve(users[USER_DB]);
+		})
+	})
 }
--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ module.exports = function(app){
     ******************/
 
     var helloWorld = require('../controllers/HelloWorldController');
+    var User = require('../controllers/UserController');
 
     /******************
     * HELLOWORLD ROUTE*
@@ -16,7 +17,23 @@ module.exports = function(app){
             res.status(200).send(helloWorld[0]);
         });
     });
-    app.get('/login', function(req, res){
-        
+    app.post('/login', function(req, res){
+        User.login(req.body, function(err, userDB){
+            if (err) {
+                var error = {
+                    errorCode: err.errorCode,
+                    errorKey: err.errorKey
+                }
+                return res.status(err.status).send(error);
+            }
+            var returnUser = {
+                token: userDB._id,
+                language: userDB.language,
+                email: userDB.email,
+                firstname: userDB.firstname,
+                lastname: userDB.lastname
+            }
+            res.status(200).send(returnUser);
+        })
     });
 }
