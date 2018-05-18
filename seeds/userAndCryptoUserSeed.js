e629e1c1823a845cad0a6e8643bdb3559560199d
fmolins <frankii267mc@gmail.com>
2018-05-18 12:04:11 +0200

    Modified seed file

--- seeds/userAndCryptoUserSeed.js
+++ seeds/userAndCryptoUserSeed.js
@@ -1,4 +1,7 @@
 var User = require('../models/User');
+var AccountGroup = require('../models/AccountGroup');
+var Account = require('../models/Account');
+var Session = require('../models/Session');
 var CryptoUser = require('../models/CryptoUser');
 
 var user = new User(
@@ -10,44 +13,6 @@ var user = new User(
         email: "test@test.test",
         password:"testpasswd",
         language: "javascript",
-        accountGroups:
-            [{
-                _id: 1,
-                image: "",
-                name: "accountGroupTest",
-                accounts:
-                    [{
-                        _id: 1,
-                        title: "Facebook",
-                        image: "test",
-                        description: "descriptiontest",
-                        user: "facebookUser",
-                        password: "ThisIsMyPassword"
-                    },
-                    {
-                        _id: 2,
-                        title: "Facebook",
-                        image: "test",
-                        description: "descriptiontest",
-                        user: "facebookUser",
-                        password: "ThisIsMyPassword"
-                    }
-                ]
-            },
-            {
-                _id: 2,
-                image: "",
-                name: "accountGroupTest",
-                accounts:
-                    [{
-                        _id: 1,
-                        title: "Facebook",
-                        image: "test",
-                        description: "descriptiontest",
-                        user: "facebookUser",
-                        password: "ThisIsMyPassword"
-                    }]
-            }]
     }
 );
 
@@ -56,10 +21,90 @@ user.save(function (err, user) {
     console.log("guardat");
 });
 
-var millisecondsToWait = 500;
+var millisecondsToWait = 1000;
 setTimeout(function() {
     User.findOne({email: "test@test.test"},function (err, user2) {
       if (err) return console.error(err);
+      var accountGroup = new AccountGroup (
+          {
+              userGroupId: 1,
+              userId: user2._id,
+              image: "",
+              name: "accountGroupTest",
+          }
+      );
+      accountGroup.save(function (err, accountGroup) {
+          if (err) return console.error(err);
+            console.log("guardat group");
+      });
+      var account = new Account(
+          {
+              userGroupId: 1,
+              userId: user2._id,
+              title: "Facebook",
+              image: "test",
+              description: "descriptiontest",
+              user: "facebookUser",
+              password: "ThisIsMyPassword"
+          }
+      );
+      account.save(function (err, account) {
+          if (err) return console.error(err);
+            console.log("guardat account");
+      });
+      var account2 = new Account(
+          {
+              userGroupId: 1,
+              userId: user2._id,
+              title: "Facebook",
+              image: "test",
+              description: "descriptiontest",
+              user: "facebookUser",
+              password: "ThisIsMyPassword"
+          }
+      );
+      account2.save(function (err, account2) {
+          if (err) return console.error(err);
+            console.log("guardat account2");
+      });
+      var accountGroup2 = new AccountGroup(
+          {
+              userGroupId: 2,
+              userId: user2._id,
+              image: "",
+              name: "accountGroupTest",
+          }
+      );
+      accountGroup2.save(function (err, accountGroup2) {
+          if (err) return console.error(err);
+            console.log("guardat group2");
+      });
+      var account3 = new Account(
+          {
+              userGroupId: 2,
+              userId: user2._id,
+              title: "Facebook",
+              image: "test",
+              description: "descriptiontest",
+              user: "facebookUser",
+              password: "ThisIsMyPassword"
+          }
+      );
+      account3.save(function (err, account3) {
+          if (err) return console.error(err);
+            console.log("guardat account3");
+      });
+      var session = new Session({
+          userId: user2._id,
+          ip: "192.168.1.1",
+          location: "Somewhere",
+          browser: "shitfox",
+          os: "win",
+      });
+      session.save(function (err, session) {
+          if (err) return console.error(err);
+            console.log("guardat session");
+      });
       var cryptoUser = new CryptoUser({
           userid: user2._id,
           privatekey: "thisisnotprivatekey"
