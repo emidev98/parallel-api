a4f19ea07cec53d587897878e573a4ace80eb123
Emi Harastasan <eharastasan@gmail.com>
2018-05-19 15:24:21 +0200

    HOTFIX :> remove uselsess else ;

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -16,23 +16,23 @@ module.exports.hasAccess = function(req, res, next){
 	if(_.contains(anonymousAllowedRoutes, req.url.substring(1, 5))){
 		req.url = req.url.replace(replaceUrlText[AUTH], "");
         next();
-    } else {
-		req.url = req.url.replace(replaceUrlText[PORTAL], "");
-		var authentication = req.get('Authentication');
-		var authenticationString = Buffer.from(authentication, 'base64').toString('utf8');
-		var authenticationComponents = authenticationString.split("|");
-	    var tokenString = authenticationComponents[0];
-	    var emailString = authenticationComponents[1];
-		User.isLogged(tokenString, emailString, function(err, user){
-			if(err){
-				var errorStatus = {
-					errorCode: err.errorCode,
-					errorKey: err.errorKey
-				}
-				return res.status(err.status).send(errorStatus);
-			}
-			req.headers.email = emailString;
-			next();
-		});
     }
+	
+	req.url = req.url.replace(replaceUrlText[PORTAL], "");
+	var authentication = req.get('Authentication');
+	var authenticationString = Buffer.from(authentication, 'base64').toString('utf8');
+	var authenticationComponents = authenticationString.split("|");
+    var tokenString = authenticationComponents[0];
+    var emailString = authenticationComponents[1];
+	User.isLogged(tokenString, emailString, function(err, user){
+		if(err){
+			var errorStatus = {
+				errorCode: err.errorCode,
+				errorKey: err.errorKey
+			}
+			return res.status(err.status).send(errorStatus);
+		}
+		req.headers.email = emailString;
+		next();
+	});
 }
