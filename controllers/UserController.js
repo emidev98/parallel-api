0f944360c11f220ebfc7762fe157e9c33aeee8f9
fmolins <frankii267mc@gmail.com>
2018-06-01 10:23:30 +0200

    Changed recovery token expire time

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -291,7 +291,7 @@ module.exports.sendMailResetPassword = function(userEmail, callback){
 }
 
 module.exports.resetPassword = function(user, callback){
-    var TIMEOUT = 50000;
+    var TIMEOUT = 600000;
     if (user.token.toString() == ""){
         return callback(new CustomError(errorCodes.INCORRECT_TOKEN), undefined)
     }
